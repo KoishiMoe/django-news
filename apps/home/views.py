1982572d87ac1bc7bb6c63830148f38812c02d98
KoishiMoe <68314080+KoishiMoe@users.noreply.github.com>
2023-04-24 01:45:02 +0800

    homepage: use slide

--- apps/home/views.py
+++ apps/home/views.py
@@ -8,10 +8,10 @@ from posts.models import Post
 
 def home(request):
     # get 21 posts at most to reduce database query
-    posts = Post.objects.filter(approved=True).order_by('-created_time')[:21]
+    posts = Post.objects.filter(approved=True).order_by('-created_time')[:25]
     if posts:
-        featured = posts[0]
-        latest = posts[1:]
-        return render(request, 'front/home.html', context={'featured': featured, 'latest': latest})
+        featured_posts = posts[:min(5, len(posts))]
+        latest = posts[5:] if len(posts) > 5 else None
+        return render(request, 'front/home.html', context={'latest': latest, 'featured_posts': featured_posts})
     else:
-        return render(request, 'front/home.html', context={'featured': None, 'latest': None})
+        return render(request, 'front/home.html', context={'latest': None, 'featured_posts': None})

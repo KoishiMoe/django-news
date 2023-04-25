ef9825701b3539a4c49e2fd8a9f7ba4642126cdb
KoishiMoe <68314080+KoishiMoe@users.noreply.github.com>
2023-04-25 17:40:15 +0800

    fix error when commenting without login

--- apps/comments/views.py
+++ apps/comments/views.py
@@ -8,6 +8,8 @@ from comments.models import Comment
 
 
 def comment_add(request, id):
+    if not(request.user and request.user.is_authenticated):
+        return redirect(reverse('login'))
     Comment.objects.create(content=request.POST.get('content'),
                            author=request.user,
                            post_id=id)
